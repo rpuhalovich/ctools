df7766c0088f228dfb516bc503d6d6f2911890e0
Ryan Puhalovich <reanpuhalovich@gmail.com>
2025-01-07 20:11:27 +1100

    Remove include keyword

--- scripts/clang-tools.sh
+++ scripts/clang-tools.sh
@@ -5,10 +5,9 @@ cd tmp
 
 if [ ! -f "./LLVM-19.1.6-macOS-ARM64.tar.xz" ]; then
     wget "https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.6/LLVM-19.1.6-macOS-ARM64.tar.xz"
+    tar -xf "LLVM-19.1.6-macOS-ARM64.tar.xz"
+    cp "LLVM-19.1.6-macOS-ARM64/bin/clang-format" .
+    cp "LLVM-19.1.6-macOS-ARM64/bin/clang-tidy" .
 fi
-
-tar -xf "LLVM-19.1.6-macOS-ARM64.tar.xz"
-cp "LLVM-19.1.6-macOS-ARM64/bin/clang-format" .
-cp "LLVM-19.1.6-macOS-ARM64/bin/clang-tidy" .
 
 cd ..
