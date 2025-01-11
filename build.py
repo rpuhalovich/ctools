08379ddebb8cd4c1a930415bd5ff569705ed409e
Ryan Puhalovich <reanpuhalovich@gmail.com>
2025-01-10 19:14:48 -0800

    Implement install-ctools

--- build.py
+++ build.py
@@ -1,9 +1,42 @@
-import subprocess as sp
+import shutil
+import os
+import subprocess
 import sys
 
-def main(args: list[str]):
-    for string in args:
-        if string == "build":
-            sp.call(["cmake", "--build", "build"])
+def exe(cmd: str) -> None:
+    subprocess.call(cmd.split());
 
-main(sys.argv)
+def mkdir(path: str) -> None:
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def cp(source: str, destination: str) -> None:
+    shutil.copy(source, destination)
+
+def main(args: list[str]) -> None:
+    if args[0] == "build":
+        exe("cmake --build build")
+
+    if args[0] == "proj":
+        exe("cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug")
+
+    if args[0] == "release":
+        exe("cmake -S . -B release -DCMAKE_BUILD_TYPE=Release")
+        exe("cmake --build release --config Release")
+
+    if args[0] == "install-ctools":
+        main(["release"])
+        mkdir("bin")
+        if os.name == "nt":
+            cp("./release/Release/ctemplate.exe", "./bin/ctemplate.exe")
+            cp("./release/Release/cenum.exe", "./bin/cenum.exe")
+        else:
+            cp("./release/ctemplate", "./bin/ctemplate")
+            cp("./release/cenum", "./bin/cenum")
+
+    if args[0] == "clean":
+        shutil.rmtree("bin")
+        shutil.rmtree("build")
+        shutil.rmtree("release")
+
+main(sys.argv[1:])
