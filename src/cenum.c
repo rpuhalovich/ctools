8ccc15579be07b134ce7a2cda9463e5cef3c64e9
Ryan Puhalovich <reanpuhalovich@gmail.com>
2025-01-18 20:02:46 +1100

    Add ivalue enum option

--- src/cenum.c
+++ src/cenum.c
@@ -8,20 +8,34 @@
 #define MAX_ENUMS 128
 #define MAX_ENUM_VALUES 128
 
-unsigned char* pool;
-unsigned char* ptr;
-
 char cfile[MAX_FILE_STRLEN];
 char hfile[MAX_FILE_STRLEN];
 char hfile_ifndef[MAX_FILE_STRLEN];
 
-int decl_len = 0;
-char decl[MAX_ENUMS][MAX_STRLEN];
-char* decl_values[MAX_ENUM_VALUES][MAX_ENUM_VALUES];
-int decl_values_lens[MAX_ENUM_VALUES];
+typedef struct {
+    char* name;
+    char* label;
+    int val;
+} Value;
 
-char prefix[MAX_ENUMS][MAX_ENUM_VALUES];
-char* optional_values[MAX_ENUM_VALUES][MAX_STRLEN];
+typedef struct {
+    char* name;
+    char* prefix;
+    Value* values;
+    size_t valueslen;
+} Enum;
+
+Enum* enums;
+size_t enumslen;
+
+unsigned char* pool;
+unsigned char* ptr;
+void* allocate(size_t size)
+{
+    void* resptr = ptr;
+    ptr += size;
+    return resptr;
+}
 
 int read_enum_file(char* path)
 {
@@ -35,17 +49,16 @@ int read_enum_file(char* path)
     while (fgets(line, sizeof(line), f) != NULL) {
         int linelen = strlen(line);
 
-        int i = 0;
-        for (; line[i] != ' ' && i < linelen; i++)
-            ;
+        int spaceidx = 0;
+        for (; line[spaceidx] != ' ' && spaceidx < linelen; spaceidx++);
 
         if (strncmp("CFILE", line, strlen("CFILE")) == 0)
-            memcpy(cfile, (char*)(line + i + 1), linelen - i - 2);
+            memcpy(cfile, (char*)(line + spaceidx + 1), linelen - spaceidx - 2);
 
         if (strncmp("HFILE", line, strlen("HFILE")) == 0) {
-            memcpy(hfile, (char*)(line + i + 1), linelen - i - 2);
+            memcpy(hfile, (char*)(line + spaceidx + 1), linelen - spaceidx - 2);
 
-            memcpy(hfile_ifndef, (char*)(line + i + 1), linelen - i - 2);
+            memcpy(hfile_ifndef, (char*)(line + spaceidx + 1), linelen - spaceidx - 2);
             for (int j = 0; hfile_ifndef[j] != '\0'; j++) {
                 if (hfile_ifndef[j] == '.')
                     hfile_ifndef[j] = '_';
@@ -55,32 +68,63 @@ int read_enum_file(char* path)
         }
 
         if (strncmp("ENUM", line, strlen("ENUM")) == 0) {
-            int j = i + 1;
-            for (; line[j] != ' ' && j < linelen; j++)
-                ;
+            int spaceidx2 = spaceidx + 1;
+            for (; line[spaceidx2] != ' ' && spaceidx2 < linelen; spaceidx2++);
 
-            memcpy(decl[decl_len], (char*)(line + i + 1), j - i - 1);
-            memcpy(prefix[decl_len], (char*)(line + j + 1), linelen - j - 2);
+            Enum e = {0};
+            e.values = allocate(sizeof(Value) * MAX_ENUM_VALUES);
 
-            decl_len++;
-            cur_value_index = 0;
+            e.name = allocate(sizeof(char) * MAX_STRLEN);
+            memcpy(e.name, (char*)(line + spaceidx + 1), spaceidx2 - spaceidx - 1);
+
+            e.prefix = allocate(sizeof(char) * MAX_STRLEN);
+            memcpy(e.prefix, (char*)(line + spaceidx2 + 1), linelen - spaceidx2 - 2);
+
+            enums[enumslen++] = e;
         }
 
         if (strncmp("VALUE", line, strlen("VALUE")) == 0) {
-            int j = i + 1;
-            for (; line[j] != ' ' && j < linelen; j++)
-                ;
+            int spaceidx2 = spaceidx + 1;
+            for (; line[spaceidx2] != ' ' && spaceidx2 < linelen; spaceidx2++);
 
-            int cur_decl_index = decl_len - 1;
-            if (j == linelen) {
-                memcpy(decl_values[cur_decl_index][cur_value_index], (char*)(line + i + 1), linelen - i - 2);
+            Value v = {0};
+            v.name = allocate(sizeof(char) * MAX_STRLEN);
+            v.label = allocate(sizeof(char) * MAX_STRLEN);
+
+            if (spaceidx2 == linelen) {
+                memcpy(v.name, (char*)(line + spaceidx + 1), linelen - spaceidx - 2);
             } else {
-                memcpy(decl_values[cur_decl_index][cur_value_index], (char*)(line + i + 1), j - i - 1);
-                memcpy(optional_values[cur_decl_index][cur_value_index], (char*)(line + j + 1), linelen - j - 2);
+                memcpy(v.name, (char*)(line + spaceidx + 1), spaceidx2 - spaceidx - 1);
+                memcpy(v.label, (char*)(line + spaceidx2 + 1), linelen - spaceidx2 - 2);
             }
 
-            decl_values_lens[cur_decl_index]++;
-            cur_value_index++;
+            enums[enumslen - 1].values[enums[enumslen - 1].valueslen++] = v;
+        }
+
+        if (strncmp("IVALUE", line, strlen("IVALUE")) == 0) {
+            int spaceidx2 = spaceidx + 1;
+            for (; line[spaceidx2] != ' ' && spaceidx2 < linelen; spaceidx2++);
+
+            int spaceidx3 = spaceidx2 + 1;
+            for (; line[spaceidx3] != ' ' && spaceidx3 < linelen; spaceidx3++);
+
+            Value v = {0};
+
+            char valstr[MAX_STRLEN];
+            memcpy(valstr, (char*)(line + spaceidx + 1), linelen - spaceidx - 2);
+            v.val = atoi(valstr);
+
+            v.name = allocate(sizeof(char) * MAX_STRLEN);
+            v.label = allocate(sizeof(char) * MAX_STRLEN);
+
+            if (spaceidx3 == linelen) {
+                memcpy(v.name, (char*)(line + spaceidx2 + 1), linelen - spaceidx2 - 2);
+            } else {
+                memcpy(v.name, (char*)(line + spaceidx2 + 1), spaceidx3 - spaceidx2 - 1);
+                memcpy(v.label, (char*)(line + spaceidx3 + 1), linelen - spaceidx3 - 2);
+            }
+
+            enums[enumslen - 1].values[enums[enumslen - 1].valueslen++] = v;
         }
     }
 
@@ -90,6 +134,7 @@ int read_enum_file(char* path)
 
 int write_implementation(char* dir)
 {
+    /*
     char path[MAX_STRLEN];
     sprintf(path, "%s/%s", dir, cfile);
 
@@ -123,11 +168,14 @@ int write_implementation(char* dir)
     }
 
     fclose(f);
+    */
+
     return 0;
 }
 
 int write_header(char* dir)
 {
+    /*
     char path[MAX_STRLEN];
     sprintf(path, "%s/%s", dir, hfile);
 
@@ -152,42 +200,30 @@ int write_header(char* dir)
 
     fprintf(f, "#endif // %s\n", hfile_ifndef);
     fclose(f);
+    */
+
     return 0;
 }
 
 int main(int argc, char** argv)
 {
-    if (argc != 3)
-        return 1;
+    if (argc != 3) return 1;
 
-    size_t size = sizeof(unsigned char) * MAX_ENUMS * MAX_ENUM_VALUES * MAX_STRLEN * 2;
+    size_t size = 8000000; // 8MB
     pool = malloc(size);
     memset(pool, 0, size);
     ptr = pool;
 
-    for (int i = 0; i < MAX_ENUMS; i++) {
-        for (int j = 0; j < MAX_ENUM_VALUES; j++) {
-            decl_values[i][j] = (char*)ptr;
-            ptr += sizeof(char) * MAX_STRLEN;
+    enums = allocate(sizeof(Enum) * MAX_ENUMS);
 
-            optional_values[i][j] = (char*)ptr;
-            ptr += sizeof(char) * MAX_STRLEN;
-        }
-    }
-
-    int retval = 0;
-
-    retval = read_enum_file(argv[1]);
-    if (!strlen(cfile) || !strlen(hfile) || retval)
-        goto cleanup;
+    int retval = read_enum_file(argv[1]);
+    if (!strlen(cfile) || !strlen(hfile) || retval) goto cleanup;
 
     retval = write_header(argv[2]);
-    if (retval)
-        goto cleanup;
+    if (retval) goto cleanup;
 
     retval = write_implementation(argv[2]);
-    if (retval)
-        goto cleanup;
+    if (retval) goto cleanup;
 
 cleanup:
     free(pool);
